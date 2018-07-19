35fdb69dba9faa8a971b965de4dc55e2df6a16a6
PC17 <PC17@DESKTOP-BF1RU52.fritz.box>
2018-07-19 16:26:12 +0200

    some fixes

--- src/com/beer/utils/BeerEventManager.java
+++ src/com/beer/utils/BeerEventManager.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.beer.utils;
 
 import calendar.api.CalendarEvent;
@@ -16,8 +11,6 @@ import java.util.ArrayList;
 public class BeerEventManager implements calendar.api.CalendarEventDatabase {
 
     private ArrayList<BeerEvent> events = new ArrayList<>();
-    //private Scanner sc = new Scanner(System.in);
-
     private TextUtils txtUtils = TextUtils.getInstance();
 
     @Override
--- src/com/beer/utils/TextUtils.java
+++ src/com/beer/utils/TextUtils.java
@@ -14,6 +14,7 @@ public class TextUtils {
     private static TextUtils instance = new TextUtils();
     private Scanner sc = new Scanner(System.in);
 
+    //singleton pattern
     private TextUtils() {
     }
 
